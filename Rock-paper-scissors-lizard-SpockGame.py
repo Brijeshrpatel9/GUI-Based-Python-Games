c018affc0a5e459e41d4f6abe9ddf163c62bf3d0
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-04-14 17:42:47 -0500

    Create Rock-paper-scissors-lizard-SpockGame.py

---
+++ Rock-paper-scissors-lizard-SpockGame.py
@@ -0,0 +1,92 @@
+# Rock-paper-scissors-lizard-Spock template
+
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+def name_to_number(name):
+    if name == "rock":
+        return 0
+    elif name == "Spock":
+        return 1
+    elif name == "paper":
+        return 2
+    elif name == "lizard":
+        return 3
+    elif name == "scissors":
+        return 4
+    else:
+        print "Invalid thing"
+    
+    # delete the follwing pass statement and fill in your code below
+    pass
+
+
+    # convert name to number using if/elif/else
+    # don't forget to return the result!
+
+
+def number_to_name(number):
+    
+    if number == 0:
+        return "rock"
+    elif number == 1:
+        return "Spock"
+    elif number == 2:
+        return "paper"
+    elif number == 3:
+        return "lizard"
+    elif number == 4:
+        return "scissors"
+    else:
+        print "Invalid number"
+    # delete the follwing pass statement and fill in your code below
+    pass
+    
+    # convert number to a name using if/elif/else
+    # don't forget to return the result!
+    
+
+def rpsls(player_choice): 
+   
+   
+    print "Player chooses", player_choice  
+    player_number = name_to_number(player_choice)
+        
+    import random
+    comp_number = random.randint(0, 4)
+    
+    comp_choice = number_to_name(comp_number)
+    print "Computer chooses", comp_choice
+    
+    compare = (comp_number - player_number) % 5
+    
+    if compare == 1:
+        print "Computer wins!"
+    elif compare == 2:
+      print "Computer wins!"
+    elif compare == 3:
+        print "Player wins!"
+    elif compare == 4:
+        print "Player wins!"
+    else:
+        print "Player and Computer tie!"
+   
+        
+    print ""
+
+# test your code - LEAVE THESE CALLS IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
