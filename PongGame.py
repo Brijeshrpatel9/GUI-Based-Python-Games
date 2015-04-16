8cd2ff7116e9b817f89ddec574ddc191e1360e5a
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-04-16 02:50:25 -0500

    Update PongGame.py

--- PongGame.py
+++ PongGame.py
@@ -1,6 +1,8 @@
 # Pong Game with Color Ball (Ball Color changes when it touches Paddle)
 
+I have impelmented "Pong Game" in Python using Code Skulptor Tool. When Ball touches one of the paddle, it changes color. When ball misses the paddle touch, score is incremented by 1.
 
+This is "2 Player" game.
 
 # Implementation of classic arcade game Pong
 
