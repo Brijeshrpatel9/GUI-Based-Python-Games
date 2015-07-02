ee0a401b4243a2f799ebcde226c8a8e5123b3613
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-07-01 22:57:28 -0400

    Comment

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,4 +1,4 @@
-# template for "Guess the number" mini-project
+# Code for "Guess the number" mini-project
 # input will come from buttons and an input field
 # all output for the game will be printed in the console
 
