419f2c5ceae4f20e6c00f58f1dd2c5947b58a824
Brijesh Patel <Brijesh.Patel@finra.org>
2015-09-04 10:25:01 -0400

    Code refactoring

--- Spaceship.py
+++ Spaceship.py
@@ -17,6 +17,7 @@ paused = False
 
 #ImageInfo class
 class ImageInfo:
+
     def __init__(self, centre, size, radius = 0, lifespan = None, animated = False):
         self.centre = centre
         self.size = size
@@ -43,13 +44,16 @@ class ImageInfo:
         return self.animated
 
 # art assets created by Kim Lathrop, may be freely re-used in non-commercial projects, please credit Kim
+
 # debris images - debris1_brown.png, debris2_brown.png, debris3_brown.png, debris4_brown.png
 #                 debris1_blue.png, debris2_blue.png, debris3_blue.png, debris4_blue.png, debris_blend.png
 debris_info = ImageInfo([320, 240], [640, 480])
 debris_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/debris2_brown.png")
+
 # nebula images - nebula_brown.png, nebula_blue.png
 nebula_info = ImageInfo([400, 300], [800, 600])
 nebula_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_brown.png")
+
 # splash image
 splash_info = ImageInfo([200, 150], [400, 300])
 splash_image = simplegui.load_image("http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/splash.png")
